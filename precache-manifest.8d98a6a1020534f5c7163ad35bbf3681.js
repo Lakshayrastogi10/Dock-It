126ed92184a52e6523a2401cfc632f89c3fad3d6
lakshay <lakshayrastogi20154@gmail.com>
2022-04-11 21:53:53 +0530

    Updates

---
+++ precache-manifest.8d98a6a1020534f5c7163ad35bbf3681.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d0866c596dc9ef63cb0",
+    "url": "/my-app/static/js/main.2d0866c5.chunk.js"
+  },
+  {
+    "revision": "744aeaec76443519493b",
+    "url": "/my-app/static/js/1.744aeaec.chunk.js"
+  },
+  {
+    "revision": "a3bd857c0333674b1d75",
+    "url": "/my-app/static/js/runtime~main.a3bd857c.js"
+  },
+  {
+    "revision": "0988b8414c6bd289035e8eb01f292f51",
+    "url": "/my-app/index.html"
+  }
+];
